0337a46d6dc947744d254d8b6d41e54711b01e7e
User <User@USER->
2021-07-11 18:58:39 +0300

    Add project files.

---
+++ HomeTask-01.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HomeTask_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 4; //result = 16
+            int b = -5; //result = 5
+            int c = 0; //result = 0
+            
+            int x = a * a;
+            Console.WriteLine($"{x}");
+
+            int y = Math.Abs(b);
+            Console.WriteLine($"{y}");
+
+            Console.WriteLine($"{c}");
+        }
+    }
+}
